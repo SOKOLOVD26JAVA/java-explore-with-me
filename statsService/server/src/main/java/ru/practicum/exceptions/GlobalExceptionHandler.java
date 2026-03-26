b44faa381372bf253e4142cd5d536a77ef10023e
Dmitry Sokolov <wotcool1@yandex.ru>
2026-03-26 17:39:06 +0300

    1 итерация

--- statsService/server/src/main/java/ru/practicum/exceptions/GlobalExceptionHandler.java
+++ statsService/server/src/main/java/ru/practicum/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
